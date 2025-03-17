bc41b173866ad3867e9a9ad4b9b8c3c4fb87e3d4
GautamKumar <gautamajay52@gmail.com>
2025-03-17 11:52:50 +0530

    ok

--- pyrogram/session/session.py
+++ pyrogram/session/session.py
@@ -431,7 +431,7 @@ class Session:
 
                 # restart was never being called after Exception block
                 if not self.restart_event.is_set():
-                    self.loop.create_task(self.restart())
+                    self.client.loop.create_task(self.restart())
                 else:
                     # multiple Exceptions can be raised in a row, so we need to wait for the restart to finish
                     try:
